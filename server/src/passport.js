ccfa34e9cb17ea858134197da9a4d2777872c390
Ayushsharma1908 <ayushsharma192004@gmail.com>
2026-01-04 17:55:28 +0530

    Updated Passport.js

--- server/src/passport.js
+++ server/src/passport.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://hear-me-out-oa3q.onrender.com/auth/google/callback"
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
