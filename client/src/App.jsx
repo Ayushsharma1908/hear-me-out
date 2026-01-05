63003e19eae3b05ff8ba4102f8ab8ffb10a2ebcd
Ayushsharma1908 <ayushsharma192004@gmail.com>
2026-01-05 16:08:58 +0530

    updated Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from './pages/LandingPage';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import OAuthSuccess from './pages/OAuthSuccess';
 
 function App() {
   return (
